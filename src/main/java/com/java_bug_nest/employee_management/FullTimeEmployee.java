2300356eb4f3aa7bb5956fa1e7011665708f9087
NickyHariniaina <nickyharini1na@gmail.com>
2025-08-04 11:03:45 +0300

    fix: file tree and package name

---
+++ src/main/java/com/java_bug_nest/employee_management/FullTimeEmployee.java
@@ -0,0 +1,17 @@
+package com.java_bug_nest.employee_management;
+
+public class FullTimeEmployee {
+  public int employeeId;
+  public String name;
+  public double monthlySalary;
+
+  public FullTimeEmployee(int employeeId, String name, double monthlySalary) {
+    this.employeeId = employeeId;
+    this.name = name;
+    this.monthlySalary = monthlySalary;
+  }
+
+  public double calculatePay() {
+    return monthlySalary;
+  }
+}
